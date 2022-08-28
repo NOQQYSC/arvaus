804300ccc9e50bb9a158fc5217d74f9bf8f36683
Kemi <kembalos@hotmail.com>
2022-08-29 01:45:50 +0300

    valmis

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, KeyboardType, Alert } from 'react-native';
 
 export default function App() {
+  const [times, setTimes] = useState(1);
+  const [number, setNumber] = useState(0);
+  const [message, setMessage] = useState('Guess a number between 1-100');
+  const [random, setRandom] = useState(Math.floor(Math.random() * 100) + 1);
+
+const guess = () => {
+  setTimes(time => times + 1)
+  if (number == random){
+    Alert.alert('You guessed the number in ' + times + ' guesses');
+    setMessage('Guess a number between 1-100');
+    setRandom(Math.floor(Math.random() * 100) + 1);
+    setTimes(1);
+    setNumber(0);
+  }
+  else if (number < random){
+    setMessage('Your guess ' + number + ' is too low')
+  }
+  else if (number > random){
+    setMessage('Your guess ' + number + ' is too high')
+  }
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{message}</Text>
+      <TextInput style={{width: 200, borderBottomColor: 'gray', borderWidth: 1}}
+      onChangeText={numba => setNumber(numba)} value={number} keyboardType= 'numeric'/>
+      <Button onPress={guess} title="Make guess"/>
       <StatusBar style="auto" />
     </View>
   );
